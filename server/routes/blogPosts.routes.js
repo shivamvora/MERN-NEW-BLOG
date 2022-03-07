834136b17f5a4d21d06e847d727c2991b561e148
shivamvora <shivampatel27102001@gmail.com>
2022-03-07 19:43:51 +0530

    push all code

---
+++ server/routes/blogPosts.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getAllBlogPosts, addBlogPost, getSinglePost, updateSingleBlogPost, removeSingleBlogPost, likeBlogPost } from './controllers/blogPosts.controller.js';
+
+const router = express.Router();
+
+router.get( '/', getAllBlogPosts );
+router.post( "/", addBlogPost );
+router.get( "/:id", getSinglePost );
+router.patch( "/:id", updateSingleBlogPost );
+router.delete( "/:id", removeSingleBlogPost );
+router.patch( "/:id/likeedBlogPost", likeBlogPost );
+
+export default router;
---
+++ server/routes/controllers/blogPosts.controller.js
@@ -0,0 +1,120 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import BlogPost from '../../models/blogs.js';
+
+const router = express.Router();
+
+/**
+ * for get all blogs from database
+ */
+
+export const getAllBlogPosts = async ( req, res ) => {
+    try {
+        const blogPosts = await BlogPost.find();
+        res.status( 200 ).json( blogPosts )
+    } catch ( error ) {
+        res.status( 404 ).json( { message: error.message } );
+    }
+}
+
+/**
+ * for add single blog
+ */
+
+export const addBlogPost = async ( req, res ) => {
+    const { title, description, fileUpload, creator, tags } = req.body;
+
+    const createNewPost = new BlogPost( {
+        title,
+        description,
+        fileUpload,
+        creator,
+        tags
+    } )
+
+    try {
+        await createNewPost.save();
+        res.status( 201 ).json( createNewPost );
+    } catch ( error ) {
+        res.status( 409 ).json( { message: error.message } );
+    }
+}
+
+/**
+ * getSinglePost
+ */
+
+export const getSinglePost = async ( req, res ) => {
+    const { id } = req.params;
+
+    try {
+        const singlepost = await BlogPost.findById( id );
+        res.status( 200 ).json( singlepost );
+    } catch ( error ) {
+        res.status( 404 ).json( { message: error.message } );
+    }
+};
+
+/**
+ * updateSingleBlogPost
+ */
+
+export const updateSingleBlogPost = async ( req, res ) => {
+    const { id } = req.params;
+    const { title, description, creator, fileUpload, tags } = req.body;
+
+    if ( !mongoose.Types.ObjectId.isValid( id ) )
+        return res.status( 404 ).send( `post ${id} not found` );
+
+    const updatedBlogPost = {
+        creator,
+        title,
+        description,
+        tags,
+        fileUpload,
+        _id: id,
+    };
+    await BlogPost.findByIdAndUpdate( id, updatedBlogPost, { new: true } );
+    res.json( updatedBlogPost );
+};
+
+/**
+ * removeSingleBlogPost
+ */
+
+export const removeSingleBlogPost = async ( req, res ) => {
+    const { id } = req.params;
+
+    if ( !mongoose.Types.ObjectId.isValid( id ) )
+        return res.status( 404 ).send( `post ${id} not found` );
+
+    await BlogPost.findByIdAndRemove( id );
+
+    res.json( { message: "Successfully deleted" } );
+};
+
+
+/**
+ * give like
+ */
+
+export const likeBlogPost = async ( req, res ) => {
+    const { id } = req.params;
+
+    if ( !mongoose.Types.ObjectId.isValid( id ) )
+        return res.status( 404 ).send( `No post with id: ${id}` );
+
+    const post = await BlogPost.findById( id );
+
+    const updatedBlogPost = await BlogPost.findByIdAndUpdate(
+        id,
+        { upvote: post.upvote + 1 },
+        { new: true }
+    );
+
+    res.json( updatedBlogPost );
+};
+
+
+
+export default router;
